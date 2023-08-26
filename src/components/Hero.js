7905e37986a55c93ff7e65df7dc66dcae33e1f0b
pasan uvindu <pasanuvindu7@gmail.com>
2023-08-27 01:31:20 +0530

    Mini Update Of home

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,33 +1,40 @@
+// src/components/Home.jsx
 import React from "react";
-import Typed from "react-typed";
 
 const FitnessHero = () => {
   return (
-    <div className="text-white">
-      <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center">
-        <p className="text-[#00df9a] font-bold p-2">
-          ACHIEVE YOUR FITNESS GOALS
-        </p>
-        <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold md:py-6">
-          <span className="text-[#FF6B6B]">Elevate Your Health.</span>
-        </h1>
-        <p className="md:text-5xl sm:text-4xl text-xl font-bold py-4 text-black">
-          Tailored workouts and nutrition plans for{" "}
-          <Typed
-            className="inline-block"
-            strings={["Strength", "Endurance", "Wellness"]}
-            typeSpeed={120}
-            backSpeed={140}
-            loop
-          />
-        </p>
-        <p className="md:text-2xl text-xl font-bold text-gray-500">
-          Achieve your fitness goals with personalized plans for strength, endurance, and overall wellness.
-        </p>
-        <button className="bg-gradient-to-r from-[#FF6B6B] to-[#FFD56B] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black">
-          Get Started
-        </button>
-      </div>
+    <div className="bg-gray-100 min-h-screen">
+      <header className="bg-blue-500 p-6">
+        <div className="container mx-auto">
+          <h1 className="text-white text-3xl font-semibold">
+            Welcome to Fitness Pro
+          </h1>
+          <p className="mt-2 text-white">
+            Your journey to a healthier lifestyle starts here!
+          </p>
+        </div>
+      </header>
+      <main className="container mx-auto p-8">
+        <section className="grid grid-cols-1 md:grid-cols-2 gap-8">
+          <div className="bg-white p-6 rounded shadow-md">
+            <h2 className="text-xl font-semibold mb-2">Personal Training</h2>
+            <p>
+              Our experienced trainers are here to guide you through your
+              fitness journey with personalized training plans.
+            </p>
+          </div>
+          <div className="bg-white p-6 rounded shadow-md">
+            <h2 className="text-xl font-semibold mb-2">Nutrition Plans</h2>
+            <p>
+              Achieve your fitness goals faster with our expert-designed
+              nutrition plans tailored to your needs.
+            </p>
+          </div>
+        </section>
+      </main>
+      {/* <footer className="bg-gray-200 p-4 text-center">
+        <p>&copy; 2023 Fitness Pro. All rights reserved.</p>
+      </footer> */}
     </div>
   );
 };
