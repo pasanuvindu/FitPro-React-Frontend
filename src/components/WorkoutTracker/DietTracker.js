bdde5733b23298617a4bea204a886e2e498cfb2c
oshanvikasith <oshanvikasith1998@gmail.com>
2023-08-31 08:49:54 +0530

    diet tracker changes

--- src/components/WorkoutTracker/DietTracker.js
+++ src/components/WorkoutTracker/DietTracker.js
@@ -3,15 +3,14 @@ import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
-import CardHeader from "@material-ui/core/CardHeader";
-import { IconButton } from "@material-ui/core";
-import MoreIcon from "@material-ui/icons/More";
-import { Link } from "react-router-dom";
 import { makeStyles } from "@material-ui/styles";
-import { Avatar } from "@material-ui/core";
-import { blue, green, pink, yellow } from "@material-ui/core/colors";
+import { blue} from "@material-ui/core/colors";
 import DietImg from "../assets/diet.png";
 import axios from "axios";
+import LunchImg from "../assets/dish.png";
+import DinnerImg from "../assets/foodImg.png";
+import "./DietTracker.css";
+import { format } from "date-fns";
 
 const useStyles = makeStyles({
   avatar: {
@@ -30,14 +29,36 @@ function DietTracker() {
       marginLeft: "10px",
       marginRight: "191px",
     },
-    lunchTypes: {
+    Breakfast: {
       width: "351px",
       height: "154px",
       borderRadius: "5px",
       padding: "20px",
-      marginLeft: "60px",
-      marginTop: "25px",
+      marginLeft: "50px",
+      marginTop: "-255px",
       cursor: "pointer",
+      paddingLeft: "12px",
+    },
+    Lunch: {
+      width: "351px",
+      height: "154px",
+      borderRadius: "5px",
+      padding: "20px",
+      marginTop: "25px",
+      marginLeft: "-352px",
+      cursor: "pointer",
+      paddingLeft: "12px",
+    },
+    Dinner: {
+      width: "351px",
+      height: "154px",
+      borderRadius: "5px",
+      padding: "20px",
+      marginTop: "425px",
+      cursor: "pointer",
+      marginLeft: "-352px",
+      marginRight: "191px",
+      paddingLeft: "12px",
     },
     records: {
       marginRight: "35px",
@@ -82,14 +103,13 @@ function DietTracker() {
     },
   };
 
-  const classes = useStyles();
   const [records, setRecords] = useState({});
-  const [breakfastStatus, setBreakfastStatus] = useState(false);
   const [status, setStatus] = useState({});
-  const [selectedMeal, setSelectedMeal] = useState(null);
+  const today = new Date();
+  const formattedDate = format(today, "yyyy-MM-dd");
 
   useEffect(() => {
-    const apiURL = `http://localhost:5000/api/getMealProgress/2023-08-26`;
+    const apiURL = `http://localhost:5000/api/getMealProgress/${formattedDate}`;
     axios.get(apiURL).then((response) => {
       if (response.data) {
         setRecords(response.data);
@@ -99,12 +119,33 @@ function DietTracker() {
   }, []);
 
   function handleMealClick(mealType) {
-    setStatus({
-      Calories: records[0][`${mealType.toLowerCase()}Calories`],
-      Carbs: records[0][`${mealType.toLowerCase()}Carbs`],
-      Protein: records[0][`${mealType.toLowerCase()}Protein`],
-      Fats: records[0][`${mealType.toLowerCase()}Fat`],
-    });
+    if (Object.keys(records).length === 0) {
+      setStatus({
+        Calories: 0,
+        Carbs: 0,
+        Protein: 0,
+        Fats: 0,
+      });
+    } else {
+      setStatus({
+        Calories:
+          records[0][`${mealType.toLowerCase()}Calories`] !== null
+            ? records[0][`${mealType.toLowerCase()}Calories`]
+            : 0,
+        Carbs:
+          records[0][`${mealType.toLowerCase()}Carbs`] !== null
+            ? records[0][`${mealType.toLowerCase()}Carbs`]
+            : 0,
+        Protein:
+          records[0][`${mealType.toLowerCase()}Protein`] !== null
+            ? records[0][`${mealType.toLowerCase()}Protein`]
+            : 0,
+        Fats:
+          records[0][`${mealType.toLowerCase()}Fat`] !== null
+            ? records[0][`${mealType.toLowerCase()}Fat`]
+            : 0,
+      });
+    }
   }
 
   return (
@@ -115,115 +156,122 @@ function DietTracker() {
         alignItems="center"
         minHeight="100vh"
       >
-        <Card sx={styles.card}>
-          <CardContent>
-            <Typography variant="h5" component="div">
-              Meal Types
-            </Typography>
-          </CardContent>
+        {/* <CardContent>
+          <Typography variant="h5" component="div">
+            Meal Types
+          </Typography>
+        </CardContent> */}
 
-          <div className="diet-container">
-            <Card
-              elevation={4}
-              sx={styles.lunchTypes}
-              onClick={() => handleMealClick("Breakfast")}
-            >
-              <CardContent>
-                <Typography variant="body1" color="textSecondary">
-                  {/* {"Project Status :" + record.projectStatus} */}
-                  Breakfast
-                </Typography>
+        <div className="diet-container">
+          <Card
+            sx={styles.Breakfast}
+            onClick={() => handleMealClick("Breakfast")}
+          >
+            <CardContent>
+              <Typography
+                variant="body1"
+                color="textSecondary"
+                style={{
+                  fontWeight: "bold",
+                  fontSize: "20px",
+                  marginLeft: "37px",
+                }}
+              >
+                Breakfast
+              </Typography>
 
-                <div>
-                  <img
-                    src={DietImg} // Replace with the actual image URL
-                    alt="Diet Tracker"
-                    className="image-container"
-                    style={{
-                      width: "80px",
-                      height: "90px",
-                      marginTop: "-22px",
-                    }}
-                  />
-                </div>
-                <Typography variant="body1" color="textSecondary"></Typography>
-                <Typography variant="body1" color="textSecondary">
-                  {/* Burned Calories: {records.burnedCalories} */}
-                </Typography>
-                <Typography variant="body1" color="textSecondary">
-                  {/* Total Reserved Calories: {records.totalReservedCalories} */}
-                </Typography>
-              </CardContent>
-            </Card>
-          </div>
-          <div className="diet-container">
-            <Card
-              elevation={4}
-              sx={styles.lunchTypes}
-              onClick={() => handleMealClick("Lunch")}
-            >
-              <CardContent>
-                <Typography variant="body1" color="textSecondary">
-                  {/* {"Project Status :" + record.projectStatus} */}
-                  Lunch
-                </Typography>
-                <div>
-                  <img
-                    src={DietImg} // Replace with the actual image URL
-                    alt="Diet Tracker"
-                    className="image-container"
-                    style={{
-                      width: "80px",
-                      height: "90px",
-                      marginTop: "-22px",
-                    }}
-                  />
-                </div>
-                <Typography variant="body1" color="textSecondary">
-                  {/* Progress: {records.progress}% */}
-                </Typography>
-                <Typography variant="body1" color="textSecondary">
-                  {/* Burned Calories: {records.burnedCalories} */}
-                </Typography>
-                <Typography variant="body1" color="textSecondary">
-                  {/* Total Reserved Calories: {records.totalReservedCalories} */}
-                </Typography>
-              </CardContent>
-            </Card>
-          </div>
-          <div className="diet-container">
-            <Card
-              elevation={4}
-              sx={styles.lunchTypes}
-              onClick={() => handleMealClick("Dinner")}
-            >
-              <CardContent>
-                <Typography variant="body1" color="textSecondary">
-                  {/* {"Project Status :" + record.projectStatus} */}
-                  Dinner
-                </Typography>
-                <div>
-                  <img
-                    src={DietImg} // Replace with the actual image URL
-                    alt="Diet Tracker"
-                    className="image-container"
-                    style={{
-                      width: "80px",
-                      height: "90px",
-                      marginTop: "-22px",
-                    }}
-                  />
-                </div>
-                <Typography variant="body1" color="textSecondary">
-                </Typography>
-                <Typography variant="body1" color="textSecondary">
-                </Typography>
-                <Typography variant="body1" color="textSecondary">
-                </Typography>
-              </CardContent>
-            </Card>
-          </div>
-        </Card>
+              <div>
+                <img
+                  src={DietImg} // Replace with the actual image URL
+                  alt="Diet Tracker"
+                  className="image-container"
+                  style={{
+                    width: "80px",
+                    height: "90px",
+                    marginTop: "-22px",
+                  }}
+                />
+              </div>
+              <Typography variant="body1" color="textSecondary"></Typography>
+              <Typography variant="body1" color="textSecondary">
+                {/* Burned Calories: {records.burnedCalories} */}
+              </Typography>
+              <Typography variant="body1" color="textSecondary">
+                {/* Total Reserved Calories: {records.totalReservedCalories} */}
+              </Typography>
+            </CardContent>
+          </Card>
+        </div>
+        <div className="diet-container">
+          <Card sx={styles.Lunch} onClick={() => handleMealClick("Lunch")}>
+            <CardContent>
+              <Typography
+                variant="body1"
+                color="textSecondary"
+                style={{
+                  fontWeight: "bold",
+                  fontSize: "20px",
+                  marginLeft: "37px",
+                }}
+              >
+                Lunch
+              </Typography>
+              <div>
+                <img
+                  src={LunchImg} // Replace with the actual image URL
+                  alt="Diet Tracker"
+                  className="image-container"
+                  style={{
+                    width: "80px",
+                    height: "90px",
+                    marginTop: "-22px",
+                  }}
+                />
+              </div>
+              <Typography variant="body1" color="textSecondary">
+                {/* Progress: {records.progress}% */}
+              </Typography>
+              <Typography variant="body1" color="textSecondary">
+                {/* Burned Calories: {records.burnedCalories} */}
+              </Typography>
+              <Typography variant="body1" color="textSecondary">
+                {/* Total Reserved Calories: {records.totalReservedCalories} */}
+              </Typography>
+            </CardContent>
+          </Card>
+        </div>
+        <div className="diet-container">
+          <Card sx={styles.Dinner} onClick={() => handleMealClick("Dinner")}>
+            <CardContent>
+              <Typography
+                variant="body1"
+                color="textSecondary"
+                style={{
+                  fontWeight: "bold",
+                  fontSize: "20px",
+                  marginLeft: "37px",
+                }}
+              >
+                Dinner
+              </Typography>
+              <div>
+                <img
+                  src={DinnerImg} // Replace with the actual image URL
+                  alt="Diet Tracker"
+                  className="image-container"
+                  style={{
+                    width: "80px",
+                    height: "90px",
+                    marginTop: "-22px",
+                  }}
+                />
+              </div>
+              <Typography variant="body1" color="textSecondary"></Typography>
+              <Typography variant="body1" color="textSecondary"></Typography>
+              <Typography variant="body1" color="textSecondary"></Typography>
+            </CardContent>
+          </Card>
+        </div>
 
         <div>
           <Card sx={styles.records}>
@@ -260,7 +308,7 @@ function DietTracker() {
                     />
                   </div>
                   <Typography variant="body1" color="textSecondary">
-                    Calories: {status.Calories}
+                    Calories Count (g): {status.Calories}
                   </Typography>
                   <Typography variant="body1" color="textSecondary">
                     {/* Burned Calories: {records.burnedCalories} */}
@@ -276,7 +324,7 @@ function DietTracker() {
                 <CardContent>
                   <Typography variant="body1" color="textSecondary">
                     {/* {"Project Status :" + record.projectStatus} */}
-                    Carbs  Status
+                    Carbs Status
                   </Typography>
                   <div>
                     <img
@@ -291,7 +339,7 @@ function DietTracker() {
                     />
                   </div>
                   <Typography variant="body1" color="textSecondary">
-                    Calories: {status.Calories}
+                    Carbs Count (g): {status.Calories}
                   </Typography>
                   <Typography variant="body1" color="textSecondary">
                     {/* Burned Calories: {records.burnedCalories} */}
@@ -307,7 +355,7 @@ function DietTracker() {
                 <CardContent>
                   <Typography variant="body1" color="textSecondary">
                     {/* {"Project Status :" + record.projectStatus} */}
-                    Protein  Status
+                    Protein Status
                   </Typography>
                   <div>
                     <img
@@ -322,7 +370,7 @@ function DietTracker() {
                     />
                   </div>
                   <Typography variant="body1" color="textSecondary">
-                    Protein: {status.Protein}
+                    Protein Count (g): {status.Protein}
                   </Typography>
                   <Typography variant="body1" color="textSecondary">
                     {/* Burned Calories: {records.burnedCalories} */}
@@ -338,7 +386,7 @@ function DietTracker() {
                 <CardContent>
                   <Typography variant="body1" color="textSecondary">
                     {/* {"Project Status :" + record.projectStatus} */}
-                    Fats  Status
+                    Fats Status
                   </Typography>
                   <div>
                     <img
@@ -353,7 +401,7 @@ function DietTracker() {
                     />
                   </div>
                   <Typography variant="body1" color="textSecondary">
-                    Fats: {status.Fats}
+                    Fats Count (g): {status.Fats}
                   </Typography>
                   <Typography variant="body1" color="textSecondary">
                     {/* Burned Calories: {records.burnedCalories} */}
